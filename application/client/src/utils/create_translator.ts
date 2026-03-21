6525a98d3da09f9ca667285d4b0e62e6522006fe
yuto33547 <monogatari9010@gmail.com>
2026-03-21 13:51:02 +0900

    デプロイ5
    - llmをlazy load

--- application/client/src/utils/create_translator.ts
+++ application/client/src/utils/create_translator.ts
@@ -1,4 +1,3 @@
-import { CreateMLCEngine } from "@mlc-ai/web-llm";
 import { stripIndents } from "common-tags";
 import * as JSONRepairJS from "json-repair-js";
 import langs from "langs";
@@ -16,11 +15,18 @@ interface Params {
 
 export async function createTranslator(params: Params): Promise<Translator> {
   const sourceLang = langs.where("1", params.sourceLanguage);
-  invariant(sourceLang, `Unsupported source language code: ${params.sourceLanguage}`);
+  invariant(
+    sourceLang,
+    `Unsupported source language code: ${params.sourceLanguage}`,
+  );
 
   const targetLang = langs.where("1", params.targetLanguage);
-  invariant(targetLang, `Unsupported target language code: ${params.targetLanguage}`);
+  invariant(
+    targetLang,
+    `Unsupported target language code: ${params.targetLanguage}`,
+  );
 
+  const { CreateMLCEngine } = await import("@mlc-ai/web-llm");
   const engine = await CreateMLCEngine("gemma-2-2b-jpn-it-q4f16_1-MLC");
 
   return {
@@ -43,12 +49,15 @@ export async function createTranslator(params: Params): Promise<Translator> {
         temperature: 0,
       });
 
-      const content = reply.choices[0]!.message.content;
-      invariant(content, "No content in the reply from the translation engine.");
+      const content = reply.choices[0]?.message.content;
+      invariant(
+        content,
+        "No content in the reply from the translation engine.",
+      );
 
       const parsed = JSONRepairJS.loads(content);
       invariant(
-        parsed != null && "result" in parsed,
+        parsed != null && typeof parsed === "object" && "result" in parsed,
         "The translation result is missing in the reply.",
       );
 
