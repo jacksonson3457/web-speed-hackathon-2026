e430848f6c77d26f1e4470c0d6b70e29519a2034
yuto33547 <monogatari9010@gmail.com>
2026-03-21 17:37:49 +0900

    デプロイ9
    - バイナリ関数を非同期

--- application/client/src/utils/fetchers.ts
+++ application/client/src/utils/fetchers.ts
@@ -4,7 +4,6 @@ import { gzip } from "pako";
 
 export async function fetchBinary(url: string): Promise<ArrayBuffer> {
   const result = await $.ajax({
-    async: false,
     dataType: "binary",
     method: "GET",
     responseType: "arraybuffer",
@@ -15,7 +14,6 @@ export async function fetchBinary(url: string): Promise<ArrayBuffer> {
 
 export async function fetchJSON<T>(url: string): Promise<T> {
   const result = await $.ajax({
-    async: false,
     dataType: "json",
     method: "GET",
     url,
@@ -25,7 +23,6 @@ export async function fetchJSON<T>(url: string): Promise<T> {
 
 export async function sendFile<T>(url: string, file: File): Promise<T> {
   const result = await $.ajax({
-    async: false,
     data: file,
     dataType: "json",
     headers: {
@@ -44,7 +41,6 @@ export async function sendJSON<T>(url: string, data: object): Promise<T> {
   const compressed = gzip(uint8Array);
 
   const result = await $.ajax({
-    async: false,
     data: compressed,
     dataType: "json",
     headers: {
