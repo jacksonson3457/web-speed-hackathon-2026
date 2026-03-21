4b0c5e82682a4a8a77158b3b19284b7c5dd1c4d3
yuto33547 <monogatari9010@gmail.com>
2026-03-21 13:22:11 +0900

    デプロイ4
    - バンドルしたscript,css,mainが相対パスになっていたので開けずにテスト落ちていた

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -70,7 +70,7 @@ const config = {
     chunkFilename: "scripts/chunk-[contenthash].js",
     filename: "scripts/[name].[contenthash].js",
     path: DIST_PATH,
-    publicPath: "auto",
+    publicPath: "/",
     clean: true,
   },
 
